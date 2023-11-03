0930c445a8c6635ddad436ddc880dca40df27628
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-03 16:12:12 -0700

    Register models to admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Project, Channel, Event
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(Channel)
+admin.site.register(Event)
