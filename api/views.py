b3c64541e7087f6a11e4705e24bc3052f4baf700
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-03 16:48:05 -0700

    Init api.views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Channel, Event, Project
+from .serializers import ChannelSerializer, EventSerializer, ProjectSerializer
+
+
+class ProjectAPIView(APIView):
+    pass
+
+
+class ChannelAPIView(APIView):
+    pass
+
+
+class EventAPIView(APIView):
+    pass
