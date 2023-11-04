e6280e0460bb648b5dd10123eb559945ddc3823b
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-04 16:04:38 -0700

    add get() method

--- api/views.py
+++ api/views.py
@@ -2,12 +2,27 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from django.http.request import HttpRequest
 from .models import Channel, Event, Project
 from .serializers import ChannelSerializer, EventSerializer, ProjectSerializer
 
 
 class ProjectAPIView(APIView):
-    pass
+    # check if user is auth
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request: HttpRequest, *args, **kwargs) -> Response:
+        """Get all projects for user
+
+        Args:
+            request (HttpRequest): incoming http request
+
+        Returns:
+            Response: user projects serialized in json
+        """
+        projects = Project.objects.filter(user=request.user.id)
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ChannelAPIView(APIView):
