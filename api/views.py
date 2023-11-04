29457d8415e4f4af980cfb51e91a66b3dfbe775a
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-04 16:21:47 -0700

    Add post() method for Projects

--- api/views.py
+++ api/views.py
@@ -24,6 +24,27 @@ class ProjectAPIView(APIView):
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def post(self, request: HttpRequest, *args, **kwargs) -> Response:
+        """Post project for user
+
+        Args:
+            request (HttpRequest): incoming http request
+
+        Returns:
+            Response: http status code
+        """
+        data = {
+            "name": request.data.get("name"),
+            "user": request.user.id,
+        }
+        # serialize and validate data
+        serializer = ProjectSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ChannelAPIView(APIView):
     pass
