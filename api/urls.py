839e1ed21a65d8ffe5370473e39095c90b59ed46
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-04 17:16:39 -0700

    move auth to copycat.urls

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,5 @@ from django.urls import include, path
 from .views import ProjectAPIView
 
 urlpatterns = [
-    path("auth/", include("rest_framework.urls")),
     path("project/", ProjectAPIView.as_view()),
 ]
