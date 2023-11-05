410082268dadc7f26995de8baca34dfcba053dac
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-04 19:03:50 -0700

    Add channel view

--- api/views.py
+++ api/views.py
@@ -47,7 +47,43 @@ class ProjectAPIView(APIView):
 
 
 class ChannelAPIView(APIView):
-    pass
+    # check if user is auth
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request: HttpRequest, *args, **kwargs) -> Response:
+        """Get all channels for a user
+
+        Args:
+            request (HttpRequest): incoming http request
+
+        Returns:
+            Response: user channels serialized in json
+        """
+        channels = Channel.objects.filter(user=request.user.id)
+        serializer = Channel(channels, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request: HttpRequest, *args, **kwargs) -> Response:
+        """Post channel for user
+
+        Args:
+            request (HttpRequest): incoming http request
+
+        Returns:
+            Response: http status code
+        """
+        data = {
+            "project_id": request.data.get("project_id"),
+            "name": request.data.get("name"),
+            "user": request.user.id,
+        }
+        # serialize and validate data
+        serializer = ChannelSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class EventAPIView(APIView):
