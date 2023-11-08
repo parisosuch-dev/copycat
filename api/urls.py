03e0a8f05032db320829ac1a4350f840aee2f8d5
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-07 16:51:44 -0800

    Add log/<str:project>/ endpoint to urls

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,11 @@ from django.urls import include, path
 
 from .views.project_view import ProjectAPIView
 from .views.channel_view import ChannelAPIView
-from .views.event_view import EventAPIView
+from .views.event_view import EventAPIView, ProjectEventsView
 
 urlpatterns = [
     path("project/", ProjectAPIView.as_view()),
     path("channel/", ChannelAPIView.as_view()),
     path("log/", EventAPIView.as_view()),
+    path("log/<str:project>/", ProjectEventsView.as_view()),
 ]
